684975468e4f318b38a39d00b54fd03cc57cacda
hendhyhutomo <hendhyhutomo@gmail.com>
2023-04-05 03:32:50 +0700

    Update Product (Mobile Not Yet)

--- js/result.js
+++ js/result.js
@@ -1,26 +1,51 @@
 $(document).ready(function () {
-  $('#slider').slick({
-    dots: true,
-    arrows: false,
-    autoplay: false,
-    pauseOnHover: false,
-    fade: false,
-    easing: 'ease',
-    speed: 600,
-  });
+  // $('#slider').slick({
+  //   dots: true,
+  //   arrows: false,
+  //   autoplay: false,
+  //   pauseOnHover: false,
+  //   fade: false,
+  //   easing: 'ease',
+  //   speed: 600,
+  //   centerMode: true,
+  //   variableWidth: true,
+  //   responsive: [
+  //     {
+  //       breakpoint: 800,
+  //       settings: "unslick",
+  //     },
+  //     {
+  //       breakpoint: 600,
+  //       settings: {
+  //         slidesToShow: 2,
+  //         slidesToScroll: 2,
+  //       },
+  //     },
+  //     {
+  //       breakpoint: 480,
+  //       settings: {
+  //         slidesToShow: 1,
+  //         slidesToScroll: 1,
+  //       },
+  //     },
+  //     // You can unslick at a given breakpoint now by adding:
+  //     // settings: "unslick"
+  //     // instead of a settings object
+  //   ],
+  // });
 
-  $('[data-space]').each(function () {
-    var $this = $(this),
-      $space = $this.attr('data-space');
+  // $('[data-space]').each(function () {
+  //   var $this = $(this),
+  //     $space = $this.attr('data-space');
 
-    $('.slick-slide').css({
-      marginLeft: $space + 'px',
-      marginRight: $space + 'px',
-    });
+  //   $('.slick-slide').css({
+  //     marginLeft: $space + 'px',
+  //     marginRight: $space + 'px',
+  //   });
 
-    $('.slick-list').css({
-      marginLeft: -$space + 'px',
-      marginRight: -$space / 2 + 'px',
-    });
-  });
+  //   $('.slick-list').css({
+  //     marginLeft: -$space + 'px',
+  //     marginRight: -$space / 2 + 'px',
+  //   });
+  // });
 });
